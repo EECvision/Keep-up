1f6c81a8bfe06e35878af4b7b849b2686250ac73
Emmanuel <eec.studies@gmail.com>
2021-01-07 07:18:42 +0100

    Updates

---
+++ precache-manifest.3ffb5b3e19ad351d720cf90d7884a963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117a69c04b8a2a128cbe1512517215c5",
+    "url": "/keep_up/index.html"
+  },
+  {
+    "revision": "e68d9df9a81e7e3de074",
+    "url": "/keep_up/static/css/main.d9cb1617.chunk.css"
+  },
+  {
+    "revision": "841037b6acd9a4d68bc1",
+    "url": "/keep_up/static/js/2.619b9ec8.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/keep_up/static/js/2.619b9ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68d9df9a81e7e3de074",
+    "url": "/keep_up/static/js/main.db6e2ff0.chunk.js"
+  },
+  {
+    "revision": "697f85b5c9250f3f0e04",
+    "url": "/keep_up/static/js/runtime-main.84de60b6.js"
+  }
+]);
